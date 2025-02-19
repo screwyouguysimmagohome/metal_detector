99b7926fa9a7814736178f8be1f81c17261afef8
Jonas Bo <jonasbo@hotmail.com>
2025-02-19 01:18:31 +0100

    update test working, verified

--- dl_test.py
+++ dl_test.py
@@ -7,6 +7,9 @@ import scipy as sp
 import grid3DFunc as grid3d
 import scipy.integrate as integrate
 
+mu0 = 4*np.pi*1e-7  # vaccum permeability
+I = 10
+
 
 def define_circle(radius, definition):
     """
@@ -21,16 +24,16 @@ def define_circle(radius, definition):
       a point (x, y, z) on the circle. The circle lies in the XY plane (z=0 for all points).
     """
     step_size = 360/definition
-    phi = np.arange(0, 360+step_size, step_size)*np.pi/180
+    phi = np.linspace(0, 2*np.pi, definition+1)
     x_angles = np.cos(phi)*radius
     y_angles = np.sin(phi)*radius
     z_angles = np.zeros(x_angles.shape)*radius
-    circle = np.column_stack((x_angles, y_angles, z_angles))
+    circle = np.column_stack((np.cos(phi)*radius, np.sin(phi)*radius, 0*phi))
 
     return circle
 
 
-circle_points = define_circle(1, 15)
+circle_points = define_circle(0.02, 180)
 
 
 def point_to_point_circle(circle_points):
@@ -42,9 +45,44 @@ def point_to_point_circle(circle_points):
     return point_vecs
 
 
-point_to_point_vecs = point_to_point_circle(circle_points)
+x = 0.00
+y = 0.00
+z = 0.03
+X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
+point = np.stack((X, Y, Z), axis=-1)
 
 
+def EM_at_point(point, wire):
+    B = np.zeros((point.shape), dtype=float)
+    dl_wire = wire[1:, :3]-wire[:-1, :3]
+    dl_center = (wire[1:, :3]+wire[:-1, :3])/2
+
+    for dl, wire_seg_center in zip(dl_wire, dl_center):
+
+        dl_center_to_point = point-wire_seg_center
+        dl_center_to_point_mag = np.linalg.norm(
+            dl_center_to_point, axis=-1, keepdims=True)
+
+        cross_product = np.cross(
+            dl, dl_center_to_point)/dl_center_to_point_mag**3
+        # breakpoint()
+        B += (mu0/(4*np.pi))*I*cross_product
+
+    return B
+
+
+digital_EM = EM_at_point(point, circle_points)
+print(digital_EM)
+print(np.linalg.norm(digital_EM))
+
+theory_EM = 1*(mu0*I*0.02**2)/(2*(0.02**2+0.03**2)**(3/2))
+print(theory_EM)
+print(np.linalg.norm(theory_EM))
+
+# point_to_point_vecs = point_to_point_circle(circle_points)
+
+
+'''
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(121, projection='3d')
 ax1 = fig.add_subplot(122, projection='3d')
@@ -95,4 +133,4 @@ ax1.set_zlim([-1.2, 1.2])
 ax1.set_title("wire circle 2")
 ax1.legend()
 
-plt.show()
+plt.show()'''
